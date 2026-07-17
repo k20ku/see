fd12c2168386c9eef456f35324fe66b1d895ef32
k20ku <heizhaokuan@gmail.com>
2026-07-17 23:01:15 +0900

    feat(server): graceful shutdown by monitoring SIGTERM and SIGINT

--- main.go
+++ main.go
@@ -6,17 +6,27 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/k20ku/see/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
+	// OS signal handler
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// init config
 	cfg, err := config.New()
 	if err != nil {
 		return fmt.Errorf("see server get config: %v", err)
 	}
 
+	// listen on port
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		return fmt.Errorf("see server: failed to listen on port %d: %v", cfg.Port, err)
@@ -27,6 +37,9 @@ func run(ctx context.Context) error {
 		// Addrフィールドは使用しない
 		// Addr: server's port addr will be injected from the given net.Listener as args
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("see server: accepted request from %s.\n", r.URL.Path[1:])
+			// コマンドラインでSIGINTを実験するため
+			time.Sleep(5 * time.Second)
 			if _, err := fmt.Fprintf(w,
 				"Hello %s!\nYour User-Agent: %s\n", r.URL.Path[1:], r.Header.Get("User-Agent"),
 			); err != nil {
@@ -34,13 +47,12 @@ func run(ctx context.Context) error {
 				log.Printf("see server: failed to respond to %s.\n", r.URL.Path[1:])
 				return
 			}
-			log.Printf("see server: accepted request from %s.\n", r.URL.Path[1:])
+			log.Printf("see server responds to %s.\n", r.URL.Path[1:])
 		}),
 	}
 
-	eg, ctx := errgroup.WithContext(ctx)
-
 	// start the http request handle in another goroutine!
+	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
 		// http.ErrServerClosed は異常系ではない
 		// なぜならhttp.Server.Shutdownは正常に終了したことを示すので
@@ -52,6 +64,8 @@ func run(ctx context.Context) error {
 
 		return nil
 	})
+
+	log.Printf("see server: listen on port %d", cfg.Port)
 
 	// Wait for an exit signal from the channel
 	<-ctx.Done()
